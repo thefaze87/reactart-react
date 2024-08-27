7d8e09862e960ec8d21e943b2e6ecaa8e60f1ceb
Mark Fasel <markfasel@gmail.com>
2024-08-27 11:44:24 -0400

    using styled components in auth input

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,4 +1,37 @@
 import { useState } from 'react'
+import { styled } from 'styled-components'
+
+const ControlContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-bottom: 1.5rem;
+`
+const Label = styled.label`
+    display: block;
+    margin-bottom: 0.5rem;
+    font-size: 0.75rem;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: #6b7280;
+`
+const Input = styled.input`
+    width: 100%;
+    padding: 0.75rem 1rem;
+    line-height: 1.5;
+    background-color: #d1d5db;
+    color: #374151;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
+
+const ActionsContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    gap: 1rem;
+`
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState('')
@@ -22,10 +55,10 @@ export default function AuthInputs() {
 
     return (
         <div id="auth-inputs">
-            <div className="controls">
+            <ControlContainer>
                 <p>
-                    <label>Email</label>
-                    <input
+                    <Label>Email</Label>
+                    <Input
                         type="email"
                         className={emailNotValid ? 'invalid' : undefined}
                         onChange={(event) =>
@@ -34,8 +67,8 @@ export default function AuthInputs() {
                     />
                 </p>
                 <p>
-                    <label>Password</label>
-                    <input
+                    <Label>Password</Label>
+                    <Input
                         type="password"
                         className={passwordNotValid ? 'invalid' : undefined}
                         onChange={(event) =>
@@ -43,15 +76,15 @@ export default function AuthInputs() {
                         }
                     />
                 </p>
-            </div>
-            <div className="actions">
+            </ControlContainer>
+            <ActionsContainer>
                 <button type="button" className="text-button">
                     Create a new account
                 </button>
                 <button className="button" onClick={handleLogin}>
                     Sign In
                 </button>
-            </div>
+            </ActionsContainer>
         </div>
     )
 }
